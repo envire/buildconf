f0391ab0f09177b8be6d2a6a09ad9aec782f4fa4
Raul.Dominguez <raul.dominguez@dfki.de>
2018-02-26 18:11:36 +0100

    Adaptation for installation in cdff: - Don't install the tutorials - Instruction to set the path for installation

--- init.rb
+++ init.rb
@@ -29,6 +29,8 @@ Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'ht
 Autoproj.gitorious_server_configuration('SPACEGIT', 'git.hb.dfki.de', :fallback_to_http => false, default: 'ssh,ssh', disabled_methods: 'http,git' )
 Autoproj.gitorious_server_configuration('DFKIGIT', 'git.hb.dfki.de', :fallback_to_http => false, default: 'ssh,ssh', disabled_methods: 'http,git')
 
+Autoproj.workspace.prefix_dir = 'TO_SET_BY_INSTALLER'
+
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
 
 # This option shows the full log output. Please don't activate this on default.
